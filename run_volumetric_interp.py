8cb34cde107fb526e4c490c83f04b73527bded88
Leslie Lamarche <leslie.lamarche@sri.com>
2019-07-30 15:24:11 +0000

    Modified run script so the config file can be specified at command line.

--- run_volumetric_interp.py
+++ run_volumetric_interp.py
@@ -1,16 +1,60 @@
 # run_volumetric_interp.py
 
-# import datetime as dt
+import datetime as dt
 from Fit import Fit
+
+config_file_help = """Calculate coefficients for volmetric interpolation
+of a scalar quantity in a fitted AMISR file.
+
+Requires a configuration file containing the following example format:
+
+[DEFAULT]
+
+# parameter to be interpolated
+PARAM = 'dens'
+
+# input AMISR fitted filename
+FILENAME = '20171119.001_lp_1min-fitcal.h5'
+
+# output filename to save the coefficients to
+OUTPUTFILENAME = 'test_out.h5'
+
+# number of radial base functions used
+MAXK = 4
+
+# order of the spherical cap harmionics expansion
+MAXL = 6
+
+# limit of the cap (degrees)
+CAP_LIM = 6
+
+# list of regularization methods to use (options are '0thorder' and 'curvature')
+REGULARIZATION_LIST = ['0thorder']
+
+# the method that should be used to determine the regularization parameter
+REGULARIZATION_METHOD = 'chi2'
+
+# the maximum z value to use in calculating the integrals for the 0th order regularization matricies
+MAX_Z_INT = inf
+"""
 
 def main():
 
-#     st = dt.datetime(2017,11,21,18,40)
-#     et = dt.datetime(2017,11,21,18,50)
+    from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+    # Build the argument parser tree
+    parser = ArgumentParser(description=config_file_help,
+                            formatter_class=RawDescriptionHelpFormatter)
+    arg = parser.add_argument('config_file',help='A configuration file.')
+   
+    args = vars(parser.parse_args())
+
+    st = dt.datetime(2017,11,21,18,40)
+    et = dt.datetime(2017,11,21,18,50)
     
-    fit = Fit('config.ini')
-#     fit.fit(st, et)
-    fit.fit()
+    fit = Fit(args['config_file'])
+    fit.fit(st, et)
+#     fit.fit()
     fit.saveh5()
 
 	
