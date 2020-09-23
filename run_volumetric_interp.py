ace39d7473acfe34e6e3c76f1b008f13a5d06be0
Leslie Lamarche <leslie.lamarche@sri.com>
2020-09-23 10:21:34 -0700

    Update README.

--- run_volumetric_interp.py
+++ run_volumetric_interp.py
@@ -10,13 +10,34 @@ Requires a configuration file containing the following example format:
 [DEFAULT]
 
 # parameter to be interpolated
-PARAM = 'dens'
+PARAM = dens
 
 # input AMISR fitted filename
-FILENAME = '20171119.001_lp_1min-fitcal.h5'
+FILENAME = 20161127.002_lp_1min-fitcal.h5
 
 # output filename to save the coefficients to
-OUTPUTFILENAME = 'test_out.h5'
+OUTPUTFILENAME = test_out.h5
+
+# list of regularization methods to use (options are '0thorder' and 'curvature')
+REGULARIZATION_LIST = curvature
+
+# the method that should be used to determine the regularization parameter
+REGULARIZATION_METHOD = chi2
+
+# only consider points with errors between these limits
+ERRLIM = 1e10,1e13
+
+# only consider points with these fit codes
+GOODFITCODE = 1,2,3,4
+
+# only consider points with chi-squared values in this range
+CHI2LIM = 0.1,10
+
+
+[MODEL]
+# Which model to use
+MODEL = Model
+; MODEL = Model_RBF
 
 # number of radial base functions used
 MAXK = 4
@@ -25,16 +46,25 @@ MAXK = 4
 MAXL = 6
 
 # limit of the cap (degrees)
-CAP_LIM = 6
-
-# list of regularization methods to use (options are '0thorder' and 'curvature')
-REGULARIZATION_LIST = ['0thorder']
-
-# the method that should be used to determine the regularization parameter
-REGULARIZATION_METHOD = 'chi2'
+CAP_LIM = 10
 
 # the maximum z value to use in calculating the integrals for the 0th order regularization matricies
-MAX_Z_INT = inf
+MAX_Z_INT = INF
+
+LATCP = 78
+
+LONCP = 262
+
+EPS = 100000.0
+
+LATRANGE = 74,80
+
+LONRANGE = 260,285
+
+ALTRANGE = 100,600
+
+NUMGRIDPNT = 7
+
 """
 
 def main():
@@ -45,15 +75,14 @@ def main():
     parser = ArgumentParser(description=config_file_help,
                             formatter_class=RawDescriptionHelpFormatter)
     arg = parser.add_argument('config_file',help='A configuration file.')
-   
+
     args = vars(parser.parse_args())
 
     fit = Fit(args['config_file'])
     fit.fit()
     fit.saveh5()
 
-	
-	
+
+
 if __name__ == '__main__':
 	main()
-	
