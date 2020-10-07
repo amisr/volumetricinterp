c6a9420ee38fd743ec5970a8a202b838cb48360f
Leslie Lamarche <leslie.lamarche@sri.com>
2020-10-07 13:13:44 -0700

    Read config file parameters directly from example config file.

--- run_volumetricinterp.py
+++ run_volumetricinterp.py
@@ -2,79 +2,21 @@
 
 from interpolate import Interpolate
 
-config_file_help = """Calculate coefficients for volmetric interpolation
-of a scalar quantity in a fitted AMISR file.
+description = "Calculate coefficients for volmetric interpolation of a scalar quantity in a fitted AMISR file."
 
-Requires a configuration file containing the following example format:
+with open('example_config.ini', 'r') as f:
+    config_file_help = f.readlines()
 
-[DEFAULT]
+config_file_help = 'A configuration file that specifies the following parameters:\n'+''.join([line for line in config_file_help if not line.startswith('#') and len(line.strip())>0])
 
-# parameter to be interpolated
-PARAM = dens
-
-# input AMISR fitted filename
-FILENAME = 20161127.002_lp_1min-fitcal.h5
-
-# output filename to save the coefficients to
-OUTPUTFILENAME = test_out.h5
-
-# list of regularization methods to use (options are '0thorder' and 'curvature')
-REGULARIZATION_LIST = curvature
-
-# the method that should be used to determine the regularization parameter
-REGULARIZATION_METHOD = chi2
-
-# only consider points with errors between these limits
-ERRLIM = 1e10,1e13
-
-# only consider points with these fit codes
-GOODFITCODE = 1,2,3,4
-
-# only consider points with chi-squared values in this range
-CHI2LIM = 0.1,10
-
-
-[MODEL]
-# Which model to use
-MODEL = Model
-; MODEL = Model_RBF
-
-# number of radial base functions used
-MAXK = 4
-
-# order of the spherical cap harmionics expansion
-MAXL = 6
-
-# limit of the cap (degrees)
-CAP_LIM = 10
-
-# the maximum z value to use in calculating the integrals for the 0th order regularization matricies
-MAX_Z_INT = INF
-
-LATCP = 78
-
-LONCP = 262
-
-EPS = 100000.0
-
-LATRANGE = 74,80
-
-LONRANGE = 260,285
-
-ALTRANGE = 100,600
-
-NUMGRIDPNT = 7
-
-"""
 
 def main():
 
-    from argparse import ArgumentParser, RawDescriptionHelpFormatter
+    from argparse import ArgumentParser, RawTextHelpFormatter
 
     # Build the argument parser tree
-    parser = ArgumentParser(description=config_file_help,
-                            formatter_class=RawDescriptionHelpFormatter)
-    arg = parser.add_argument('config_file',help='A configuration file.')
+    parser = ArgumentParser(description=description, formatter_class=RawTextHelpFormatter)
+    arg = parser.add_argument('config_file',help=config_file_help)
 
     args = vars(parser.parse_args())
 
