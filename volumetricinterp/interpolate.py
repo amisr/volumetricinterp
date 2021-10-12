43448b274a19d3ea92469858e04559c25babfff8
Leslie Lamarche <leslie.lamarche@sri.com>
2021-10-12 12:06:42 -0700

    Move specification of IRI to config file.

--- volumetricinterp/interpolate.py
+++ volumetricinterp/interpolate.py
@@ -85,6 +85,12 @@ class Interpolate(object):
         self.chi2lim = [float(i) for i in config.get('DEFAULT', 'CHI2LIM').split(',')]
         self.goodfitcode = [int(i) for i in config.get('DEFAULT', 'GOODFITCODE').split(',')]
 
+        self.use_iri = config.getboolean('DEFAULT', 'USE_IRI')
+        self.iri_lat_grid = [float(i) for i in config.get('DEFAULT', 'IRI_LAT_GRID').split(',')]
+        self.iri_lon_grid = [float(i) for i in config.get('DEFAULT', 'IRI_LON_GRID').split(',')]
+        self.iri_alt_grid = [float(i) for i in config.get('DEFAULT', 'IRI_ALT_GRID').split(',')]
+        self.iri_err = config.getfloat('DEFAULT', 'IRI_ERR')
+
         self.model_name = config.get('MODEL', 'NAME')
 
 
@@ -502,7 +508,6 @@ class Interpolate(object):
             self.error = self.error[idx]
 
 
-        self.iri_lat, self.iri_lon, self.iri_alt, self.iri_dens, self.iri_error = self.get_iri(self.utime)
 
         # print(self.iri_dens.shape, np.argwhere(np.isnan(self.iri_dens)))
 
@@ -510,6 +515,9 @@ class Interpolate(object):
         # from scipy.spatial import Delaunay
         # hull = Delaunay(self.hull_vert)
         # outside_points = np.argwhere(hull.find_simplex(np.array([iri_x,iri_y,iri_z]).T)>=0)[0]
+
+        if self.use_iri:
+            self.iri_lat, self.iri_lon, self.iri_alt, self.iri_dens, self.iri_error = self.get_iri(self.utime)
 
 
         # loop over every record and calculate the coefficients
@@ -552,19 +560,30 @@ class Interpolate(object):
             # iri_alt0 = iri_alt[np.isfinite(iri)]
             # iri0 = iri[np.isfinite(iri)]
 
-            print(self.lat.shape, self.iri_lat.shape)
+            # print(self.lat.shape, self.iri_lat.shape)
 
-            lat0 = np.concatenate((self.lat.flatten(),self.iri_lat.flatten()))
-            lon0 = np.concatenate((self.lon.flatten(),self.iri_lon.flatten()))
-            alt0 = np.concatenate((self.alt.flatten(),self.iri_alt.flatten()))
-            ne0 = np.concatenate((self.value[i].flatten(),self.iri_dens[i].flatten()))
-            er0 = np.concatenate((self.error[i].flatten(),self.iri_error[i].flatten()))
+            if self.use_iri:
+                lat0 = np.concatenate((self.lat.flatten(),self.iri_lat.flatten()))
+                lon0 = np.concatenate((self.lon.flatten(),self.iri_lon.flatten()))
+                alt0 = np.concatenate((self.alt.flatten(),self.iri_alt.flatten()))
+                ne0 = np.log10(np.concatenate((self.value[i].flatten(),self.iri_dens[i].flatten())))
+                er0 = np.log10(np.concatenate((self.error[i].flatten(),self.iri_error[i].flatten())))
+            else:
+                lat0 = self.lat.flatten()
+                lon0 = self.lon.flatten()
+                alt0 = self.alt.flatten()
+                ne0 = np.log10(self.value[i].flatten())
+                er0 = np.log10(self.error[i].flatten())
 
             lat0 = lat0[np.isfinite(ne0)]
             lon0 = lon0[np.isfinite(ne0)]
             alt0 = alt0[np.isfinite(ne0)]
             er0 = er0[np.isfinite(ne0)]
             ne0 = ne0[np.isfinite(ne0)]
+
+            # import matplotlib.pyplot as plt
+            # plt.hist(np.log10(ne0), bins=100, range=(1,11.9))
+            # plt.show()
 
 
             # lat0 = iri_gdlat0.flatten()
@@ -707,7 +726,8 @@ class Interpolate(object):
         # Each column correpsonds to a different "check" condition
         # TRUE for "GOOD" point; FALSE for "BAD" point
         # A "good" record that shouldn't be removed should be TRUE for EVERY check condition
-        data_check = np.array([error>self.errlim[0], error<self.errlim[1], chi2>self.chi2lim[0], chi2<self.chi2lim[1], np.isin(fitcode,self.goodfitcode)])
+        # data_check = np.array([error>self.errlim[0], error<self.errlim[1], chi2>self.chi2lim[0], chi2<self.chi2lim[1], np.isin(fitcode,self.goodfitcode)])
+        data_check = np.array([chi2>self.chi2lim[0], chi2<self.chi2lim[1], np.isin(fitcode,self.goodfitcode)])
 
         # If ANY elements of data_check are FALSE, flag index as bad data
         bad_data = np.squeeze(np.any(data_check==False,axis=0,keepdims=True))
@@ -726,14 +746,16 @@ class Interpolate(object):
 
     def get_iri(self, utime):
 
-        # print('IRI')
-
         from iri2016.base import IRI
-        altstart = 100.
-        altstop = 800.
-        altstep = 25.
+
+        # altstart = 75.
+        # altstop = 800.
+        # altstep = 25.
+        altstart = self.iri_alt_grid[0]
+        altstop = self.iri_alt_grid[1]
+        altstep = self.iri_alt_grid[2]
         # iri_alt = np.arange(altstart, altstop+altstep, altstep)
-        iri_gdlat, iri_gdlon, iri_alt = np.meshgrid(np.arange(74., 83., 1.5), np.arange(-110., -55., 7.), np.arange(altstart, altstop+altstep, altstep))
+        iri_gdlat, iri_gdlon, iri_alt = np.meshgrid(np.arange(self.iri_lat_grid[0],self.iri_lat_grid[1],self.iri_lat_grid[2]), np.arange(self.iri_lon_grid[0],self.iri_lon_grid[1],self.iri_lon_grid[2]), np.arange(altstart, altstop+altstep, altstep))
         # iri_gdlat0, iri_gdlon0 = np.meshgrid(np.arange(74., 83., 1.), np.arange(-110., -55., 5.))
         # iri_gdlat, iri_gdlon = np.meshgrid(np.array([74.,75.,82.,83.]), np.array([-110.,-105.,-60.,-55.]))
         # iri_gdlat = iri_gdlat0[(iri_gdlat0<76.) | (iri_gdlat0>80.) | (iri_gdlon0<-100.) | (iri_gdlon0>-70.)]
@@ -751,14 +773,16 @@ class Interpolate(object):
                 iri[t,idx] = out.ne
 
         # set IRI errors
-        iri_err = np.full(iri.shape, 1.e11)
-        iri_err[:,(iri_alt<200.) | (iri_alt>500.)] = 1.e10
+        # iri_err = iri.copy()*0.7
+        iri_err = np.full(iri.shape, self.iri_err)
+        # iri_err[:,(iri_alt<200.) | (iri_alt>500.)] = 1.e12
 
         iri_gdlat = iri_gdlat.flatten()
         iri_gdlon = iri_gdlon.flatten()
         iri_alt = iri_alt.flatten()
         iri = iri.reshape(iri.shape[0], -1)
         iri_err = iri_err.reshape(iri_err.shape[0], -1)
+
 
         # iri[(iri_gdlat0>76.) & (iri_gdlat0<80.) & (iri_gdlon0>-100.) & (iri_gdlon0<-70.) & ()]
 
--- volumetricinterp/validate.py
+++ volumetricinterp/validate.py
@@ -106,16 +106,16 @@ class Validate(object):
         raw_time = np.array([dt.datetime.utcfromtimestamp(t) for t in np.mean(self.interp.utime, axis=1)])
 
         # setup figure
-        fig = plt.figure(figsize=(len(self.altitudes)*2,len(raw_time)*2))
+        fig = plt.figure(figsize=(len(self.altitudes)*5,len(raw_time)*5))
         gs = gridspec.GridSpec(len(raw_time), len(self.altitudes))
-        gs.update(left=0.05,right=0.9,bottom=0.01,top=0.95)
+        gs.update(left=0.03,right=0.95,bottom=0.005,top=0.98,wspace=0.05,hspace=0.05)
         map_proj = ccrs.LambertConformal(central_latitude=np.nanmean(hull_lat), central_longitude=np.nanmean(hull_lon))
 
 
         for i, time in enumerate(raw_time):
 
             dens = est_param(time,gdlat, gdlon, gdalt, check_hull=False)
-            dens[dens<0.] = np.nan
+            # dens[dens<0.] = np.nan
 
             # iri = np.empty(iri_alt.shape)
             # for idx in np.ndindex(iri_alt.shape[:-1]):
@@ -129,7 +129,7 @@ class Validate(object):
                 # rlat = self.interp.lat[tuple(np.arange(self.interp.alt.shape[0])),tuple(aidx)]
                 # rlon = self.interp.lon[tuple(np.arange(self.interp.alt.shape[0])),tuple(aidx)]
                 # rdens = self.interp.value[i,tuple(np.arange(self.interp.alt.shape[0])),tuple(aidx)]
-                aidx = np.argwhere(np.abs(self.interp.alt - alt)<10.).flatten()
+                aidx = np.argwhere(np.abs(self.interp.alt - alt)<30.).flatten()
                 # print(len(aidx))
                 rlat = self.interp.lat[aidx]
                 rlon = self.interp.lon[aidx]
@@ -140,10 +140,8 @@ class Validate(object):
                 # irilat = self.interp.iri_lat[:,:,iriidx]
                 # irilon = self.interp.iri_lon[:,:,iriidx]
                 # iridens = self.interp.iri_dens[i,:,:,iriidx]
-                aidx = np.argwhere(np.abs(self.interp.iri_alt - alt)<10.).flatten()
-                irilat = self.interp.iri_lat[aidx]
-                irilon = self.interp.iri_lon[aidx]
-                iridens = self.interp.iri_dens[i,aidx]
+
+                # print(j, rdens, iridens)
 
                 # create plot
                 ax = fig.add_subplot(gs[i,j], projection=map_proj)
@@ -152,20 +150,28 @@ class Validate(object):
 
                 # plot density contours from RISR
                 c = ax.contourf(gdlon[:,:,j], gdlat[:,:,j], dens[:,:,j], np.linspace(self.colorlim[0],self.colorlim[1],31), extend='both', transform=ccrs.PlateCarree())
+                c.cmap.set_over('lightgrey')
+                c.cmap.set_under('dimgrey')
+
                 # print('RISR LOCATION', np.nanmean(rlat), np.nanmean(rlon))
                 # ax.scatter(rlon, rlat, c='white', s=20, transform=ccrs.Geodetic())
-                ax.scatter(rlon, rlat, c=rdens, s=10, vmin=self.colorlim[0], vmax=self.colorlim[1], transform=ccrs.Geodetic())
+                ax.scatter(rlon, rlat, c=np.log10(rdens), s=10, vmin=self.colorlim[0], vmax=self.colorlim[1], transform=ccrs.Geodetic())
                 # c=ax.scatter(rlon, rlat, c=rdens, s=10, vmin=0., vmax=1., transform=ccrs.Geodetic())
                 # ax.scatter(irilon[np.argwhere(np.isnan(iridens)).flatten()], irilat[np.argwhere(np.isnan(iridens)).flatten()], c='hotpink', s=20, transform=ccrs.Geodetic())
                 # ax.scatter(irilon, irilat, c=iridens, s=10, vmin=1.e13, vmax=2.e13, transform=ccrs.Geodetic())
-                ax.scatter(irilon, irilat, c=iridens, s=10, vmin=self.colorlim[0], vmax=self.colorlim[1], transform=ccrs.Geodetic())
+                if self.interp.use_iri:
+                    aidx = np.argwhere(np.abs(self.interp.iri_alt - alt)<10.).flatten()
+                    irilat = self.interp.iri_lat[aidx]
+                    irilon = self.interp.iri_lon[aidx]
+                    iridens = self.interp.iri_dens[i,aidx]
+                    ax.scatter(irilon, irilat, c=np.log10(iridens), s=10, vmin=self.colorlim[0], vmax=self.colorlim[1], transform=ccrs.Geodetic())
                 # ax.scatter(irilon, irilat, c='pink', s=10, transform=ccrs.Geodetic())
                 ax.set_title('{} km'.format(alt))
 
             # plot time labels and colorbars
             pos = ax.get_position()
-            plt.text(0.03,(pos.y0+pos.y1)/2.,time.time(),rotation='vertical',verticalalignment='center',horizontalalignment='center',transform=fig.transFigure)
-            cax = fig.add_axes([0.91,pos.y0,0.03,pos.height])
+            plt.text(0.015,(pos.y0+pos.y1)/2.,time.time(),rotation='vertical',verticalalignment='center',horizontalalignment='center',transform=fig.transFigure)
+            cax = fig.add_axes([0.955,pos.y0,0.01,pos.height])
             cbar = plt.colorbar(c, cax=cax)
             cbar.set_label(r'Ne (m$^{-3}$)')
 
